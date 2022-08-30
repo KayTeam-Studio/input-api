347b396aa7426eb26f3716ea64730aedfeb656e8
SirOswaldo <DeveloperOswaldo@gmail.com>
2022-08-30 14:59:16 -0300

    InventoryCloseListener.java updated

--- src/main/java/org/kayteam/inputapi/listeners/InventoryCloseListener.java
+++ src/main/java/org/kayteam/inputapi/listeners/InventoryCloseListener.java
@@ -1,18 +1,13 @@
 package org.kayteam.inputapi.listeners;
 
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
-import org.bukkit.inventory.ItemStack;
 import org.kayteam.inputapi.InputManager;
 import org.kayteam.inputapi.inputs.InventoryInput;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 public class InventoryCloseListener implements Listener {
@@ -42,39 +37,7 @@ public class InventoryCloseListener implements Listener {
 
         if ( ! title.equals(inventoryInput.getTitle()))   return;
 
-        Inventory inventory = event.getInventory();
-
-        HashMap<Integer, ItemStack> items = getItems(inventoryInput);
-
-        if ( ! inventoryInput.onCloseInventory(inventory, items))   return;
-
-        player.openInventory(inventory);
-
-    }
-
-    private HashMap<Integer, ItemStack> getItems(InventoryInput inventoryInput) {
-
-        HashMap<Integer, ItemStack> items = new HashMap<>();
-
-        List<Integer> disabledSlots = inventoryInput.getDisabledSlots();
-
-        Inventory inventory = inventoryInput.getInventory();
-
-        for (int i = 0; i < (inventoryInput.getRows() * 9); i++) {
-
-            if (disabledSlots.contains(i))   continue;
-
-            ItemStack itemStack = inventory.getItem(i);
-
-            if (itemStack == null)   continue;
-
-            if (itemStack.getType().equals(Material.AIR))   continue;
-
-            items.put(i, itemStack);
-
-        }
-
-        return items;
+        inventoryInput.onCloseInventory(inventoryInput);
 
     }
 
