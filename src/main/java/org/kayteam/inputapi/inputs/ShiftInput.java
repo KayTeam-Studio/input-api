97d3909e0342a62bec27dfaa0b5bfd7449b6269a
SirOswaldo <DeveloperOswaldo@gmail.com>
2022-08-30 17:12:14 -0300

    ShiftInput.java comments added

--- src/main/java/org/kayteam/inputapi/inputs/ShiftInput.java
+++ src/main/java/org/kayteam/inputapi/inputs/ShiftInput.java
@@ -3,5 +3,11 @@ package org.kayteam.inputapi.inputs;
 import org.bukkit.entity.Player;
 
 public interface ShiftInput {
+
+    /**
+     * Executed when the player toggle sneak.
+     * @param player the player.
+     */
     void onShift(Player player);
+
 }
