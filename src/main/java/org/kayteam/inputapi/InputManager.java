94a74ca1893fc2da29ce5d3f2b93ea63f09b98dd
SirOswaldo <DeveloperOswaldo@gmail.com>
2022-08-29 23:09:03 -0300

    PlayerQuitListener.java implemented

--- src/main/java/org/kayteam/inputapi/InputManager.java
+++ src/main/java/org/kayteam/inputapi/InputManager.java
@@ -12,10 +12,7 @@ import org.bukkit.event.player.PlayerToggleSneakEvent;
     import org.bukkit.plugin.PluginManager;
     import org.bukkit.plugin.java.JavaPlugin;
     import org.kayteam.inputapi.inputs.*;
-    import org.kayteam.inputapi.listeners.AsyncPlayerChatListener;
-    import org.kayteam.inputapi.listeners.BlockBreakListener;
-    import org.kayteam.inputapi.listeners.PlayerDropItemListener;
-    import org.kayteam.inputapi.listeners.PlayerToggleSneakListener;
+    import org.kayteam.inputapi.listeners.*;
 
     import java.util.HashMap;
 import java.util.UUID;
@@ -28,7 +25,9 @@ public class InputManager implements Listener {
     private final HashMap<UUID, ShiftInput> shifts = new HashMap<>();
 
     public InputManager(JavaPlugin javaPlugin) {
+
         this.javaPlugin = javaPlugin;
+
     }
 
     public void registerManager() {
@@ -45,52 +44,86 @@ public class InputManager implements Listener {
 
         pluginManager.registerEvents(new PlayerToggleSneakListener(this), javaPlugin);
 
+        pluginManager.registerEvents(new PlayerQuitListener(this), javaPlugin);
+
     }
 
     public JavaPlugin getJavaPlugin() {
+
         return javaPlugin;
+
     }
 
     public HashMap<UUID, BlockBreakInput> getBlocks() {
+
         return blocks;
+
     }
 
     public HashMap<UUID, ChatInput> getChats() {
+
         return chats;
+
     }
 
     public HashMap<UUID, DropInput> getDrops() {
+
         return drops;
+
     }
 
     public HashMap<UUID, ShiftInput> getShifts() {
+
         return shifts;
+
     }
 
     public void addInput(Player player, BlockBreakInput input) {
+
         blocks.put(player.getUniqueId(), input);
+
     }
+
     public void addInput(Player player, ChatInput input) {
+
         chats.put(player.getUniqueId(), input);
+
     }
+
     public void addInput(Player player, DropInput input) {
+
         drops.put(player.getUniqueId(), input);
+
     }
+
     public void addInput(Player player, ShiftInput input) {
+
         shifts.put(player.getUniqueId(), input);
+
     }
 
     public boolean isBlockInput(Player player) {
+
         return blocks.containsKey(player.getUniqueId());
+
     }
+
     public boolean isChatInput(Player player) {
+
         return chats.containsKey(player.getUniqueId());
+
     }
+
     public boolean isDropInput(Player player) {
+
         return drops.containsKey(player.getUniqueId());
+
     }
+
     public boolean isShiftInput(Player player) {
+
         return shifts.containsKey(player.getUniqueId());
+
     }
 
 }
